bf1cf4b15f683cfc012a8c4fb8b42544cf28dd65
unknown <quangnx10>
2021-02-08 14:54:02 +0700

    init project

---
+++ src/main/java/com/example/testCICD/CICDConstroller.java
@@ -0,0 +1,18 @@
+package com.example.testCICD;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CICDConstroller {
+
+    @GetMapping("/test")
+    public ResponseEntity test(){
+        ResponseEntity responseEntity = new ResponseEntity("123", HttpStatus.OK);
+        return responseEntity;
+    }
+
+}
---
+++ src/test/java/com/example/testCICD/TestCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
